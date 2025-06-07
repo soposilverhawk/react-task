e83613a0720f0e16653c325964cc367f71863258
soposilverhawk <109133065+soposilverhawk@users.noreply.github.com>
2025-06-07 18:26:15 +0400

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-task/",
+  base: '/react-task/',
   plugins: [react()],
 });
